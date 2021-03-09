691e8311c7d11cafcd6f5051afca15f023ee68f0
moyongkexing <moyongkexing@gmail.com>
2021-03-09 22:25:54 +0900

    ListComponentテストを作成

--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import { RenderInput } from './RenderInput';
+import RenderInput from './RenderInput/RenderInput';
+import FrameworkList from './FrameworkList/FrameworkList';
 
 function App() {
   const output = (text) => {
     console.log(text);
   };
+  const data = [
+    {
+      id: 1,
+      item: "React",
+    },
+    {
+      id: 2,
+      item: "Angular",
+    },
+    {
+      id: 3,
+      item: "Vue",
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <RenderInput outputConsole={output}/>
+        <FrameworkList frameworks={data}/>
+        {/* <FrameworkList /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
